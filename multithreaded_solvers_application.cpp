486b57b64c35950ccd643b90b4d68ed0af6318a9
Hoang-Giang Bui <hoang-giang.bui@durham.ac.uk>
2025-12-28 20:28:19 +0100

    Improve solver check and detection

--- multithreaded_solvers_application.cpp
+++ multithreaded_solvers_application.cpp
@@ -17,6 +17,9 @@
 // Project includes
 #include "multithreaded_solvers_application.h"
 
+#ifdef MULTITHREADED_SOLVERS_APPLICATION_USE_PARDISO
+extern "C" void pardisoinit (void   *, int    *,   int *, int *, double *, int *);
+#endif
 
 namespace Kratos
 {
@@ -42,13 +45,28 @@ bool KratosMultithreadedSolversApplication::Has(const std::string& SolverName)
         return false;
 #endif
     }
-    else if (SolverName == "PardisoSolver")
+    else if (SolverName == "SuperLUMTSolver")
     {
-#ifdef MULTITHREADED_SOLVERS_APPLICATION_USE_PARDISO
+#ifdef MULTITHREADED_SOLVERS_APPLICATION_USE_SUPERLU_MT
         return true;
 #else
         return false;
 #endif
+    }
+    else if (SolverName == "PardisoSolver")
+    {
+#ifdef MULTITHREADED_SOLVERS_APPLICATION_USE_PARDISO
+        int mtype = 1;
+        void *pt[64];
+        int iparm[64];
+        double dparm[64];
+
+        int solver = 0, error = 0;
+        pardisoinit (pt, &mtype, &solver, iparm, dparm, &error);
+        if (error == 0)
+            return true;
+#endif
+        return false;
     }
 
     return false;
