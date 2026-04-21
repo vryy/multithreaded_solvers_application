f15258ec69fa56de073211100e3b46be77e94cba
Hoang-Giang Bui <hgbk2008@gmail.com>
2026-04-21 18:19:20 +0200

    Clean up

--- custom_python/add_linear_solvers_to_python.cpp
+++ custom_python/add_linear_solvers_to_python.cpp
@@ -54,10 +54,6 @@
 #include "custom_linear_solvers/pardiso_solver.h"
 #endif
 
-#ifdef MULTITHREADED_SOLVERS_APP_USE_SPECTRA
-#include "custom_eigen_solvers/spectra_eigenvalues_solver.h"
-#endif
-
 #include "custom_python/add_linear_solvers_to_python.h"
 
 namespace Kratos
@@ -65,58 +61,6 @@ namespace Kratos
 
 namespace Python
 {
-
-    #ifdef MULTITHREADED_SOLVERS_APP_USE_SPECTRA
-    boost::python::list SpectraEigenvaluesSolver_SolveLargestUnsym(SpectraEigenvaluesSolver& rDummy, CompressedMatrix& rA, const int& ne)
-    {
-        boost::python::list values;
-
-        std::vector<double> eigenvalues_real;
-        std::vector<double> eigenvalues_imag;
-        rDummy.SolveLargestUnsym(rA, ne, eigenvalues_real, eigenvalues_imag);
-
-        for (std::size_t i = 0; i < eigenvalues_real.size(); ++i)
-        {
-            boost::python::list eval;
-            eval.append(eigenvalues_real[i]);
-            eval.append(eigenvalues_imag[i]);
-            values.append(eval);
-        }
-
-        return values;
-    }
-
-    boost::python::list SpectraEigenvaluesSolver_SolveLargestSym(SpectraEigenvaluesSolver& rDummy, CompressedMatrix& rA, const int& ne)
-    {
-        boost::python::list values;
-
-        std::vector<double> eigenvalues;
-        rDummy.SolveLargestSym(rA, ne, eigenvalues);
-
-        for (std::size_t i = 0; i < eigenvalues.size(); ++i)
-        {
-            values.append(eigenvalues[i]);
-        }
-
-        return values;
-    }
-
-    boost::python::list SpectraEigenvaluesSolver_SolveSmallestSPD(SpectraEigenvaluesSolver& rDummy, CompressedMatrix& rA,
-        SpectraEigenvaluesSolver::LinearSolverType::Pointer pLinearSolver, const int& ne)
-    {
-        boost::python::list values;
-
-        std::vector<double> eigenvalues;
-        rDummy.SolveSmallestSPD(rA, pLinearSolver, ne, eigenvalues);
-
-        for (std::size_t i = 0; i < eigenvalues.size(); ++i)
-        {
-            values.append(eigenvalues[i]);
-        }
-
-        return values;
-    }
-    #endif
 
     void MultithreadedSolversApplication_AddLinearSolversToPython()
     {
@@ -300,19 +244,6 @@ namespace Python
         .def("SetEstimateMinEigenvalueByGershgorin", &ChebyshevSolverType::SetEstimateMinEigenvalueByGershgorin)
         .def("SetEstimateMaxEigenvalueByGershgorin", &ChebyshevSolverType::SetEstimateMaxEigenvalueByGershgorin)
         ;
-
-        //***************************************************************************
-        //eigenvalue solvers
-        //***************************************************************************
-
-        #ifdef MULTITHREADED_SOLVERS_APP_USE_SPECTRA
-        class_<SpectraEigenvaluesSolver, SpectraEigenvaluesSolver::Pointer, boost::noncopyable>
-        ("SpectraEigenvaluesSolver", init<>())
-        .def("SolveLargestUnsym", &SpectraEigenvaluesSolver_SolveLargestUnsym)
-        .def("SolveLargestSym", &SpectraEigenvaluesSolver_SolveLargestSym)
-        .def("SolveSmallestSPD", &SpectraEigenvaluesSolver_SolveSmallestSPD)
-        ;
-        #endif
     }
 
 }  // namespace Python.
